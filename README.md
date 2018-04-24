210befe4a147e8eb24403d1f8ecb7b0a3855aa72
bigMeow <running@vip.163.com>
2018-04-24 11:39:26 +0800

    完善demo示范、小程序上传预览忽略掉文档

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 微信小程序左划删除组件，可能是性能最好、实现最简单的方案，使用官方组件movable-area、cover-view实现
 
 全程基本稳定在60fps,预览效果：
-![image](./preview.gif)
+![image](./doc-img/preview.gif)
--- test/test.js
+++ test/test.js
@@ -5,14 +5,41 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    todoList: [
+      {
+        id: 'a1',
+        text: '你好1'
+      },
+      {
+        id: 'a2',
+        text: '你好2'
+      },
+      {
+        id: 'a3',
+        text: '你好3'
+      },
+      {
+        id: 'a4',
+        text: '你好4'
+      },
+      {
+        id: 'a5',
+        text: '你好5'
+      },{
+        id: 'a6',
+        text: '你好6'
+      }
+    ]
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+    // 默认每个选项都是关闭状态
+    this.data.todoList.forEach(todoItem => {
+      todoItem.isOpen = false
+    });
   },
 
   handleChange: function (isOpen) {
@@ -21,5 +48,18 @@ Page({
 
   handleDelete: function () {
     console.log('点击删除了')
+  },
+
+  handleSliderLeftStart: function (e) {
+    console.log('开始左滑', e.target.dataset.id)
+    this.data.todoList.forEach(todoItem => {
+      // 除了当前项，其它打开项的菜单都关闭，确保每次只有一个项可以左滑显示删除
+      if (todoItem.id !== e.target.dataset.id && todoItem.isOpen) {
+        todoItem.isOpen = false
+      }
+    });
+    this.setData({
+      todoList: this.data.todoList
+    })
   }
 })
